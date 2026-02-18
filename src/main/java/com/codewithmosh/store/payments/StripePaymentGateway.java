8ee15df1573f3b5845ed52904e82b5d19e612368
Inayat <inu@demo.com>
2026-02-18 06:00:46 +0530

    add security rule in feature and put metadata in payment intent

--- src/main/java/com/codewithmosh/store/payments/StripePaymentGateway.java
+++ src/main/java/com/codewithmosh/store/payments/StripePaymentGateway.java
@@ -30,7 +30,7 @@ public class StripePaymentGateway implements PaymentGateway {
                     .setMode(SessionCreateParams.Mode.PAYMENT)
                     .setSuccessUrl(websiteUrl + "/checkout-success?orderId=" + order.getId())
                     .setCancelUrl(websiteUrl + "/checkout-cancel")
-                    .putMetadata("order_id", order.getId().toString());
+                    .setPaymentIntentData(createPaymentIntent(order));
 
             order.getItems().forEach(item ->
                     {
@@ -47,6 +47,11 @@ public class StripePaymentGateway implements PaymentGateway {
             System.out.println(ex.getMessage());
             throw new PaymentException();
         }
+    }
+
+    private static SessionCreateParams.PaymentIntentData createPaymentIntent(Order order) {
+        return SessionCreateParams.PaymentIntentData.builder()
+                .putMetadata("order_id", order.getId().toString()).build();
     }
 
     @Override
